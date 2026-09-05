d90cba3bf6cd6a19eb1cac0fa52492b6ca54aa33
Ethantobot <ethantobot7@gmail.com>
2026-09-05 20:58:05 +0200

    Update Window.hx

--- src/lime/ui/Window.hx
+++ src/lime/ui/Window.hx
@@ -37,7 +37,7 @@ class Window
 	#if (!lime_doc_gen || (js && html5))
 	public var element(default, null):#if (js && html5) Element #else Dynamic #end;
 	#end
-	public var nativeHandle(get, null):Dynamic;
+	//public var nativeHandle(get, null):Dynamic;
 
 	/**
 	 * The current frame rate (measured in frames-per-second) of the window.
